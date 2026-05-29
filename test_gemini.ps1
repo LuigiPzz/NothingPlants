494250fec47e14bd61867e263ff6df9f39afbaf2
LuigiPzz <luigipozzi.it@gmail.com>
2026-05-29 14:52:19 +0200

    security: remove hardcoded gemini api key from test script

--- test_gemini.ps1
+++ test_gemini.ps1
@@ -1,2 +1,2 @@
-$response = Invoke-RestMethod -Uri "https://generativelanguage.googleapis.com/v1beta/models?key=AIzaSyACFDKP8jDPriyqnlj4Dz5xeqwX-pwL_6g" -Method Get
+$response = Invoke-RestMethod -Uri "https://generativelanguage.googleapis.com/v1beta/models?key=$env:GEMINI_API_KEY" -Method Get
 $response.models | Where-Object { $_.name -like "*flash*" } | Select-Object name, supportedGenerationMethods | ConvertTo-Json -Depth 5
